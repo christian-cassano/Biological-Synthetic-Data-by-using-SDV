8df1ad2d5c7dd7cfea246fe9583197b7851cc769
christian-cassano <61978528+christian-cassano@users.noreply.github.com>
2023-02-10 13:19:33 +0000

    Add files via upload

--- utilities.py
+++ utilities.py
@@ -7,16 +7,17 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 
-
-
-''' Creation of the final report. It takes 3 parameters as arguments:
-    data: an array representing the real data
-    new_data: an array representing the synthetic data
-    primary_key: string, representing the primary_key
-'''
 def report(data,new_data,primary_key = "ID"):
+    '''
+    Creation of the final report. 
+    
+    Parameters:
+    
+        data: an array representing the real data
+        new_data: an array representing the synthetic data
+        primary_key: string, representing the primary_key
+    '''
     metadata_map = creation_metadata(data,primary_key)
-
     report = QualityReport()
     report.generate(data, new_data, metadata_map)
     report.get_details(property_name='Column Shapes')
@@ -24,14 +25,23 @@ def report(data,new_data,primary_key = "ID"):
     report.get_visualization(property_name='Column Pair Trends')
 
 
-
-''' Creation of the matadeta for the SDmetrics. It takes 2 parameters as arguments:
-    data: an array representing the real data
-    primary_key: string, representing the primary_key
-'''
 def creation_metadata(data,primary_key):
+    '''
+    Creation of the metadata for the SDmetrics. 
+    
+    Parameters:
+    
+        data: a DataFrame made out of the real data
+        primary_key: string, representing the primary_key of the dataset
+
+    Returns:
+       dictionary metadata
+    '''
+    # Creation dictionary with key the column name and value the column type 
     dict_column = dict((key,value) for key,value in data.dtypes.items())
     column_information = {}
+    # Loop to construct a column_information dict with key the column name and value a dictionary 
+    # with keys: type,subtype or regex
     for el in dict_column:
         column_information[el] = {}
         if dict_column[el] == "int64":
@@ -40,69 +50,94 @@ def creation_metadata(data,primary_key):
         elif dict_column[el] == "float64":
             column_information[el]["type"] = "numerical"
             column_information[el]["subtype"] = "float"
-        else:
+        elif el == primary_key:
             column_information[el]["type"]= "id"
             column_information[el]["subtype"]=  "string"
             column_information[el]["regex"]=  "[0-9]"
-    metadata_map = {"primary_key" : "ID", "fields": column_information}
+        else:
+            column_information[el]["type"]= "categorical"  
+    # creation metadata map with 
+    # key primary_key and value primary_key of the dataset 
+    # key fields and value the map column_information
+    metadata_map = {"primary_key" : primary_key, "fields": column_information}
     return metadata_map
 
 
-
-''' Anonymization of the data. It takes 2 parameters as arguments:
-    name_of_the_fields: The name of the field that we want to anonymize
-    category_of_the_fields: The category of the field that we want to use when we generate fake values for it
-    (if we want to use it just add the paramiter anonymize_fields into the model file)
-'''
 def anonymize_fields(name_of_the_fields,category_of_the_fields):
+    '''
+    Anonymization of the data. 
+
+    Parameters:
+        name_of_the_fields: Array, the name of the field that we want to anonymize
+        category_of_the_fields: Array, the category of the field that we want to use when we generate fake values for it
+    
+    Returns:
+        dictionary with key name of the Anonymized fields and value the associated category
+    (if we want to use it just add the parameter  anonymize_fields into the config file)    
+'''
     return dict((name_of_the_fields[i],category_of_the_fields[i])  for i in range(len(name_of_the_fields)))
 
 
-
-''' Plot Real vs. Synthetic Data for column_names ('chosen'). It takes 4 parameters as arguments:
-    model: the type of model used
-    new_data: an array representing the synthetic data
-    primary_key: string, representing the primary_key
-    column_names: the colums we want to plot 
-'''
 def plotting_column(model,data,new_data,primary_key,column_names=['AOD','Neutrophils','Hemoglobin','Platelets','BMB','BMRS']):
+    '''
+    Plot Real vs. Synthetic Data for column_names 
+        
+        Parameters:
+        
+        model: string, the model name used
+        data: DataFrame, representing the real data
+        new_data: DataFrame representing the synthetic data
+        primary_key: string, representing the primary_key
+        column_names: array, the columns we want to plot 
+    '''
+    #Creation metadata by using the specific method
     metadata_map = creation_metadata(data,primary_key)
+    #Creation of n plots, with n the size of column_names
     for column_name in column_names:
-        fig = get_column_plot(
-        real_data=data,
-        synthetic_data=new_data,
-        metadata=metadata_map,
-        column_name=column_name
-        )
+        fig = get_column_plot(real_data=data,
+                              synthetic_data=new_data,
+                              metadata=metadata_map,
+                              column_name=column_name)
+        #Save the Image 
         fig.write_image('Image/'+model+'/SDV_Metrics_'+model+'_'+column_name+'.png')
-        #fig.show()
+        fig.show()
 
-
-
-''' Plot Real vs. Synthetic Data for columns_names 'Chosen_colum' and 'Gender'. It takes 4 parameters as arguments:
-    model: the type of model used
-    data: an array representing the real data
-    new_data: an array representing the synthetic data
-    primary_key: string, representing the primary_key
-    column_names: the colums we want to plot 
-'''
 def plotting_column_pair(model,data,new_data,primary_key,column_names=['AOD','Neutrophils','Hemoglobin','Platelets','BMB','BMRS']):
+    '''
+      Plot and Save Real vs. Synthetic Data for columns_names Chosen_colum and Gender
+         Parameters:
+
+            model: string, the model name used
+            data: DataFrame, representing the real data
+            new_data: DataFrame representing the synthetic data
+            primary_key: string, representing the primary_key
+            column_names: array, the columns we want to plot 
+'''
+    # Creation metadata by using the specific method
     metadata_map = creation_metadata(data,primary_key)
+    #Creation of n plots, with n the size of column_names
     for column_name in column_names:
         fig = get_column_pair_plot(data,new_data,[column_name,"Gender"],metadata_map)
+        #Save the Image
         fig.write_image('Image/'+model+'/SDV_Metrics_'+model+'_pairing_'+'Gender'+column_name+'.png')
-        #fig.show()
+        fig.show()
 
 
 
-''' Plot the synthetic data in comparison with the real data. It takes 4 parameters as arguments:
-    model: the type of model used
-    data: an array representing the real data
-    new_data: an array representing the synthetic data
-    primary_key: string, representing the primary_key
-    column_names: the colums we want to plot 
-'''
 def plotting_data_synthetic(model,data,new_data,column_names=['AOD','Neutrophils','Hemoglobin','Platelets','BMB','BMRS']):
+    '''
+    Plot and Save the synthetic data in comparison with the real data. 
+    
+     Parameters:
+
+            model: string, the model name used
+            data: DataFrame, representing the real data
+            new_data: DataFrame representing the synthetic data
+            primary_key: string, representing the primary_key
+            column_names: array, the columns we want to plot
+
+'''
+    # Creation of the Histogram with number of bins equals to 50
     for column_name in column_names:
         x_min = min(data[column_name].min(),new_data[column_name].min())
         x_max = max(data[column_name].max(),new_data[column_name].max())
@@ -115,39 +150,35 @@ def plotting_data_synthetic(model,data,new_data,column_names=['AOD','Neutrophils
     
         #print(hist)
         #print(bin_edges)
-    
-        figsize = (14,8)
-        fsize = 18
-        dpi = 300
-        plot_params = {
-            'linewidth': 0.9,
-            'markersize': 1.5,
-            'markeredgewidth': 2.0
-        }
-        plt.xlabel(column_name)
-        plt.ylabel("Frequency")
-        plt.figure(figsize=figsize)
-        
+        plt.figure(figsize=(16,8))
+        plt.title("Original vs Synthetic Data - "+column_name)
+        plt.xlabel(column_name,fontsize=18)
+        plt.ylabel("Frequency",fontsize=18)
         plt.plot(bin_edges[:-1], hist, color='red', label='Original')
         plt.plot(bin_edges[:-1], hist_new, color='blue', label=model)
-        
         plt.legend(loc='upper left')
-        plt.savefig('Image/'+model+'/comparison_'+model+'_'+column_name+'.png', dpi=dpi)
+        # Save the Image
+        plt.savefig('Image/'+model+'/comparison_'+model+'_'+column_name+'.png', dpi=300)
+        plt.show()
         plt.close()
 
 
 
-''' Creation of the Hinstogram plot for each colums of the Real data. It takes 2 parameters as arguments:
-    model: the type of model used
-    data: an array representing the real data    
-'''
 def get_histo(model,data):
-    distributions = model.get_distributions()
+    ''' 
+    Creation of the Histogram plot for each columns of the Real data. 
     
+      Parameters:
+        model:  string, the model name used
+        data: DataFrame, representing the real data    
+'''
+    distributions = model.get_distributions()
+    # Creation of histogram for each columns in the dataset
     for i in data.columns:
-        print( i)
+        print(i)
         print(data[i].value_counts())
         data[i].hist()
+        # Save the Histogram
         plt.savefig('Histo/'+i+'.png')
         plt.close()
 
